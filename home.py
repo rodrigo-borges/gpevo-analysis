70694aef4fb61099b2a4dc08b2a966621d05b3ec
Rodrigo Borges <rodrigo.sil.borges@gmail.com>
2024-06-24 08:27:09 -0300

    Try a final fix

--- home.py
+++ home.py
@@ -38,7 +38,7 @@ def get_standings_df(race_id:str, format_text:bool=False) -> pd.DataFrame:
     standings_dict:dict = races_dict[race_id]["standings"]
     _df:pd.DataFrame = pd.DataFrame.from_records(list(standings_dict.values()))
     _df = _df.rename(columns={"car_id":"Corredor", "finished":"Concluiu", "elapsed_time":"Tempo", "max_progress":"Distância"})
-    #_df = _df.sort_values(["Tempo", "Distância"], ascending=[True, False])
+    _df = _df.sort_values(["Tempo", "Distância"], ascending=[True, False])
     _df = _df.reset_index(drop=True)
     _df["Posição"] = _df.index+1
     _df = _df.set_index(_df["Corredor"])
@@ -59,7 +59,6 @@ def get_4th_time(df:pd.DataFrame) -> pd.Series:
     return df.loc[df["finished"]]["elapsed_time"].min()
 
 def get_Nth_best(series:pd.Series, n:int=0, asc:bool=True) -> float:
-    return series.max()
     return series.sort_values(ascending=asc, ignore_index=True)[n]
 
 def get_evolution_df(exp_id:str) -> pd.DataFrame:
