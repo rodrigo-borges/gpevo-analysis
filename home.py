533a962131c8bfa1373d5362ab09f29e8ac34d6f
Rodrigo Borges <rodrigo.sil.borges@gmail.com>
2024-06-24 08:33:08 -0300

    Update home.py

--- home.py
+++ home.py
@@ -51,7 +51,7 @@ def get_standings_df(race_id:str, format_text:bool=False) -> pd.DataFrame:
     if format_text:
         _df["Distância"] = _df["Distância"].apply(lambda x: f"{x:.1f}m")
         _df["Tempo"] = _df["Tempo"].apply(lambda x: f"{x:.1f}s")
-        _df["Tempo"] = _df["Tempo"].where(~_df["Tempo"].isna(), "DNF")
+        _df["Tempo"] = _df["Tempo"].where(_df["Tempo"].isna(), "DNF")
     return _df
 
 def get_min_time(df:pd.DataFrame) -> pd.Series:
